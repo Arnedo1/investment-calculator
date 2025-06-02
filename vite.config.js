fd803b25a3982a3c0549e6f6992f31b4795feff1
Jose Arnedo <arnedo1@msn.com>
2025-06-02 19:01:58 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [tailwindcss(),
     react()],
-})
+    base: '/investment-calculator/',
+    build: {
+      outDir: 'dist'
+}})
